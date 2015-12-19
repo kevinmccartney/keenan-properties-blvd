2b81d2d285262abb501d7fae033dca7caf98b2ae
Kevin McCartney <hello@kevinmccartney.is>
2015-12-18 22:40:54 -0800

    added some functions

--- functions.php
+++ functions.php
@@ -8,3 +8,14 @@ if ( !defined( 'HJI_BLVD_SCSS' ) ) {
     define( 'HJI_BLVD_SCSS', true );
 }
 
+/**
+ * Unregistering some unused sidebars from the parent theme
+ */
+if (!function_exists('hji_keenan_widgets_init')) {
+    function hji_keenan_widgets_init() {
+        unregister_sidebar( 'idx-horizontal-search');
+        unregister_sidebar( 'blvd-main-sidebarwidgets');
+        unregister_sidebar( 'blvd-topbar-sidebarwidgets');
+    }
+    add_action('widgets_init', 'hji_keenan_widgets_init', 11);
+}
