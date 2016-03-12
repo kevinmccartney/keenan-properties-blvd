b78dbcaa9d5c53a1474ee149135afe7e61916cba
Kevin McCartney <hello@kevinmccartney.is>
2016-03-12 11:16:28 -0800

    dev checkpoint

--- single.php
+++ single.php
@@ -1,8 +1,9 @@
 <?php while (have_posts()) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <?php get_template_part( 'templates/entry-single', get_post_type() ); ?>
-</article>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  		<?php get_template_part( 'templates/entry-single', get_post_type() ); ?>
+	</article>
 <?php endwhile; ?>
+
 <?php
 /**
  * grabs the buttons & development navbar if the post belongs to
